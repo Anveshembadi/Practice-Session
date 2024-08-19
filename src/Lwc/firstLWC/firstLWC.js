5a2d468521a07b30e8407f038621e44c321e064a
Anvesh <anveshembadi@gmail.com>
2024-08-19 18:52:59 +0530

    added Lwc codes

---
+++ src/Lwc/firstLWC/firstLWC.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLWC extends LightningElement {
+
+    name = 'Anvesh';
+    company = 'Salesforce';
+    designation = 'Developer';
+    salary = '33333';
+    
+
+}
